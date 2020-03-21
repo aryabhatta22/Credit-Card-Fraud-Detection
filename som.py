6957f89d30c72ae90b7caa89bd77aeba41496cf7
aryabhatta <tarun.hive@hotmail.com>
2020-03-21 14:55:34 +0530

    Fraud detection model created

--- som.py
+++ som.py
@@ -1,3 +1,42 @@
+
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+
+
+df = pd.read_csv('Credit_Card_Applications.csv')
+X = df.iloc[:, :-1].values 
+y = df.iloc[:, -1].values  
+
+
+from sklearn.preprocessing import MinMaxScaler
+sc = MinMaxScaler(feature_range = (0,1))
+X = sc.fit_transform(X)
+
+from minisom import MiniSom 
+
+som = MiniSom(x=10, y=10, input_len = 15, sigma = 1.0, learning_rate = 0.5) 
+som.random_weights_init(X)
+som.train_random(data = X, num_iteration = 100)
+
+from pylab import bone, pcolor, colorbar, plot, show
+
+bone()
+pcolor(som.distance_map().T) 
+colorbar()
+markers = ['o', 's'] 
+colors = ['r','g']
+for i,x in enumerate(X): 
+    w = som.winner(x)  
+    plot(w[0]+0.5, w[1]+0.5,
+         markers[y[i]],  
+         markeredgecolor = colors[y[i]],
+         markerfacecolor = 'None',
+         markersize = 10, 
+         markeredgewidth = 2)   
+    
+show()
+    
+mapping = som.win_map(X) 
+frauds = np.concatenate((mapping[(2,8)], mapping[(8,8)]), axis = 0) # Change the value of coordinates based on the grid image for correct values
+frauds = sc.inverse_transform(frauds) 

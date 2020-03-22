111f3b2ce38a2ceca9f4952654c1bb8b004befeb
aryabhatta <tarun.hive@hotmail.com>
2020-03-22 18:10:50 +0530

    model integrated with ANN to rank the fraud customers

--- som.py
+++ som.py
@@ -13,6 +13,8 @@ from sklearn.preprocessing import MinMaxScaler
 sc = MinMaxScaler(feature_range = (0,1))
 X = sc.fit_transform(X)
 
+                            # Part 1 - SOM
+
 from minisom import MiniSom 
 
 som = MiniSom(x=10, y=10, input_len = 15, sigma = 1.0, learning_rate = 0.5) 
@@ -38,5 +40,33 @@ for i,x in enumerate(X):
 show()
     
 mapping = som.win_map(X) 
-frauds = np.concatenate((mapping[(2,8)], mapping[(8,8)]), axis = 0) # Change the value of coordinates based on the grid image for correct values
+frauds = np.concatenate((mapping[(2,7)], mapping[(3,4)], mapping[(1,6)]), axis = 0) # Change the value of coordinates based on the grid image for correct values
 frauds = sc.inverse_transform(frauds) 
+
+                            # Part 2 - ANN
+
+customer = df.iloc[:, 1:].values
+is_fraud = np.zeros(len(df))
+for i in range(len(df)):
+    if(df.iloc[i,0] in frauds):
+        is_fraud[i] = 1
+        
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+customer = sc.fit_transform(customer)
+
+
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+
+classifier = Sequential()                   
+classifier.add(Dense(output_dim = 2, init = 'uniform', activation = 'relu', input_dim = 15 ))                                                 
+classifier.add(Dense(output_dim = 1, init = 'uniform', activation = 'sigmoid', input_dim = 15 ))
+classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'] )
+
+classifier.fit(customer, is_fraud, batch_size = 1, epochs = 2)
+
+y_pred = classifier.predict(customer)
+y_pred = np.concatenate((df.iloc[:, 0:1].values, y_pred), axis = 1) 
+y_pred = y_pred[y_pred[:, 1].argsort()]
